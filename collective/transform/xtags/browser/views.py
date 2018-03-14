f7b2b6640d2ad484761d9027f536e15e58ee0f53
Espen Moe-Nilssen <espen@medialog.no>
2018-03-14 23:59:07 +0100

    render_views

--- collective/transform/xtags/browser/views.py
+++ collective/transform/xtags/browser/views.py
@@ -14,12 +14,12 @@ class RenderFromXtags(BrowserView):
 
     def render_xtags(self, tagged_text=""):
         """Return quark xtags as a stringified HTML document."""
-        #value = self.request.tagged_text
+        value = self.request.tagged_text.encode('utf-8')
         #if not tagged_text:
         #    value = self.context.bodytext.encode('utf-8')
 
         try:
-            element_tree = to_xml(tagged-text)
+            element_tree = to_xml(value)
             serialised_xml = tostring(element_tree, encoding='utf-8')
             return serialised_xml
         except:
