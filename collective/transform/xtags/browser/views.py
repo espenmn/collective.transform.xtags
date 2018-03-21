1781b8772311d40021e2b4a18a223a429231a61f
Espen Moe-Nilssen <espen@medialog.no>
2018-03-21 17:38:59 +0100

    hacking

--- collective/transform/xtags/browser/views.py
+++ collective/transform/xtags/browser/views.py
@@ -15,8 +15,10 @@ class RenderFromXtags(BrowserView):
     def render_xtags(self, tagged_text=""):
         """Return quark xtags as a stringified HTML document."""
         tagged_text = self.request.tagged_text
-        #or self.context.qrktext
-        #xtags = self.context.qrktext.encode('utf-8')
+
+        #not sure why this is needed,
+        #but <* on same line breaks the rendering
+        tagged_text = tagged_text.replace(" <*", "\n<*")
 
         try:
             element_tree = to_xml(tagged_text.decode('utf-8'))
--- collective/transform/xtags/widgets/widget.py
+++ collective/transform/xtags/widgets/widget.py
@@ -23,7 +23,9 @@ class XtagsWidget(text.TextWidget):
         #hack to get around unicode errors
         #there must be a quicker way to do this (?)
         tagged_text = self.value.replace("\r", "")
-        tagged_text = tagged_text.replace("\<\*", "\n\<\*")
+        
+        #not sure why this is needed
+        tagged_text = tagged_text.replace(" <*", "\n<*")
 
         #or self.context.qrktext
         #encoded_text = tagged_text.encode('utf-8')
