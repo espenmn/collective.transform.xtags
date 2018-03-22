74eb9484c571eee172f3012f4fa7e90922f56906
Espen Moe-Nilssen <espen@medialog.no>
2018-03-22 18:13:05 +0100

    replaces

--- collective/transform/xtags/widgets/widget.py
+++ collective/transform/xtags/widgets/widget.py
@@ -29,10 +29,12 @@ class XtagsWidget(text.TextWidget):
         #not sure why this is needed,
         #but <* on same line breaks the rendering
         #tagged_text = tagged_text.replace("<*", "\n<*")
+        tagged_text = tagged_text.replace(": <*", ":<*")
         tagged_text = tagged_text.replace("<*", " <*")
+        tagged_text = tagged_text.replace("<*", "<")
         tagged_text = tagged_text.replace(" <*", "\n<*")
-        tagged_text = tagged_text.replace("<", "\n<")
-
+        tagged_text = tagged_text.replace(">@", "> \n@")
+        
         try:
             element_tree = to_xml(tagged_text)
             serialised_xml = tostring(element_tree, encoding='utf-8')
