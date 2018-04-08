0e494666edc23980b93c82e8b241ced6b9d0999d
Espen Moe-Nilssen <espen@medialog.no>
2018-04-08 22:09:44 +0200

    adding content types

--- collective/transform/xtags/factories.py
+++ collective/transform/xtags/factories.py
@@ -4,22 +4,23 @@ from plone.app.dexterity.factories import DXFileFactory
 from Products.CMFCore.interfaces._content import IFolderish
 from zope.component import adapter
 from zope.interface import implementer
-
+import transaction
 
 @adapter(IFolderish)
 @implementer(IDXFileFactory)
 class XTagsFileFactory(DXFileFactory):
 
     def __call__(self, name, content_type, data):
-        import pdb; pdb.set_trace()
+        
         custom_obj = self.create_custom_stuff(name, content_type, data)
         if custom_obj:
             return custom_obj
-
+            
         return super(XTagsFileFactory, self).__call__(name, content_type, data)
 
     def create_custom_stuff(self, name, content_type, data):
-        if name == 'quarktags':
+        import pdb; pdb.set_trace()
+        if name.endswith("xtg"):
             # do you own stuff here like wrap each file in a folder
-            self.qrktext = data()
+            return super(XTagsFileFactory, self).__call__(name, content_type, qrktext=data.read())
         return
