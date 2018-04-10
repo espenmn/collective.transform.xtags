22d688f233e8e79e06fea714d75665a7eed1c265
Espen Moe-Nilssen <espen@medialog.no>
2018-04-10 23:18:57 +0200

    not working on ubuntu

--- collective/transform/xtags/widgets/widget.py
+++ collective/transform/xtags/widgets/widget.py
@@ -23,7 +23,8 @@ class XtagsWidget(text.TextWidget):
 
     def get_xtags(self):
         tagged_text = self.value
-        tagged_text = tagged_text.replace("\r", "\n")
+        tagged_text = tagged_text.replace("\r", "\n", 1)
+        tagged_text = tagged_text.replace("\r", "")
         try:
             #element_tree = to_xml(tagged_text, extra_tags_to_keep={}, css=True)
             element_tree = to_xml(tagged_text.decode('utf-8'), extra_tags_to_keep={}, css=True)
